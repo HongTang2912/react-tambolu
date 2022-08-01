2f161e0bf67b8036c34918189aca55287baeb442
HongTang2912 <hongtang240@gmail.com>
2022-08-02 00:18:33 +0700

    fjhevgfdsfnds

--- components/Product_detail/CommentBlock.js
+++ components/Product_detail/CommentBlock.js
@@ -10,47 +10,42 @@ import {getCommentById} from '/public/store/ProductState'
 export default function CommentBlock({comment_id}) {
     return (
       {
-        comment_id.map((cmt, index) => 
-        
-          <Card variant="outlined" sx={{ minWidth: 320 }} key={index}>
-            {/* <CardOverflow>
-              <AspectRatio ratio="2">
-                <img
-                  src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?crop=entropy&auto=format&fit=crop&w=3270"
-                  alt=""
-                />
-              </AspectRatio>
-            </CardOverflow> */}
-            <Typography level="h2" sx={{ fontSize: 'md', mt: 2 }}>
-              User
-            </Typography>
-            <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-              Comment content
-            </Typography>
-            <CardOverflow
-              variant="soft"
-              sx={{
-                display: 'flex',
-                gap: 1.5,
-                py: 1.5,
-                px: 'var(--Card-padding)',
-                borderTop: '1px solid',
-                borderColor: 'neutral.outlinedBorder',
-                bgcolor: 'background.level1',
-              }}
-            >
-              <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-              {
-                getCommentById([cmt]).then(res => res)
-              }
+        getCommentById(comment_id ?? []).then(res => 
+          res.map((cmt, index) =>(
+
+            <Card variant="outlined" sx={{ minWidth: 320 }} key={index}>
+              
+              <Typography level="h2" sx={{ fontSize: 'md', mt: 2 }}>
+                User
               </Typography>
-              <Box sx={{ width: 2, bgcolor: 'divider' }} />
-              <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-                Time
+              <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+                Comment content
               </Typography>
-            </CardOverflow>
-          </Card>
+              <CardOverflow
+                variant="soft"
+                sx={{
+                  display: 'flex',
+                  gap: 1.5,
+                  py: 1.5,
+                  px: 'var(--Card-padding)',
+                  borderTop: '1px solid',
+                  borderColor: 'neutral.outlinedBorder',
+                  bgcolor: 'background.level1',
+                }}
+              >
+                <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
+                  {cmt}
+                </Typography>
+                <Box sx={{ width: 2, bgcolor: 'divider' }} />
+                <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
+                  Time
+                </Typography>
+              </CardOverflow>
+            </Card>
+          ))
         )
       }
-    );
+      
+    )
+    
 }
