c02b4a4052e81655158fdaf8992fc9ef2258dea7
HongTang2912 <hongtang240@gmail.com>
2022-08-06 16:20:20 +0700

    uhfuesginbcx

--- components/Product_detail/CommentBlock.js
+++ components/Product_detail/CommentBlock.js
@@ -1,49 +1,46 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Card from '@mui/joy/Card';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Typography from '@mui/joy/Typography';
-import {getCommentById} from '/public/store/ProductState'
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import { getCommentById } from "/public/store/ProductState";
 
-export default function CommentBlock({comment_block}) {
-
-
-
-  return (
-    comment_block.map((comment, index) => (
-
+export default function CommentBlock({ comment_block }) {
+  return comment_block.map((comment, index) => (
     <Card variant="outlined" sx={{ minWidth: 320 }} key={index}>
-        {console.log(block)}
-        
-        <Typography level="h2" sx={{ fontSize: 'md', mt: 2 }}>
-          User
-        </Typography>
-        <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-          Comment content
-        </Typography>
-        <CardOverflow
-          variant="soft"
-          sx={{
-            display: 'flex',
-            gap: 1.5,
-            py: 1.5,
-            px: 'var(--Card-padding)',
-            borderTop: '1px solid',
-            borderColor: 'neutral.outlinedBorder',
-            bgcolor: 'background.level1',
-          }}
+      <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
+        User
+      </Typography>
+      <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+        Comment content
+      </Typography>
+      <CardOverflow
+        variant="soft"
+        sx={{
+          display: "flex",
+          gap: 1.5,
+          py: 1.5,
+          px: "var(--Card-padding)",
+          borderTop: "1px solid",
+          borderColor: "neutral.outlinedBorder",
+          bgcolor: "background.level1",
+        }}
+      >
+        <Typography
+          level="body3"
+          sx={{ fontWeight: "md", color: "text.secondary" }}
         >
-          <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-            {comment}
-          </Typography>
-          <Box sx={{ width: 2, bgcolor: 'divider' }} />
-          <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-            Time
-          </Typography>
-        </CardOverflow>
-      </Card>
-    ))
-        
-  )
+          {comment}
+        </Typography>
+        <Box sx={{ width: 2, bgcolor: "divider" }} />
+        <Typography
+          level="body3"
+          sx={{ fontWeight: "md", color: "text.secondary" }}
+        >
+          Time
+        </Typography>
+      </CardOverflow>
+    </Card>
+  ));
 }
