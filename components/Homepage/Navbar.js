6c138c368249c14204f497a5edfe01f8d89e2409
unknown <hongtang240@gmail.com>
2022-08-11 03:54:35 +0700

    ndgfndgjg

--- components/Homepage/Navbar.js
+++ components/Homepage/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar() {
   });
   return (
     <div className={Styles.container}>
-      <div className={`bg-white ${Styles.navbar} flex flex-col`}>
+      <div className={`bg-white ${Styles.navbar} flex flex-col container:flex-row`}>
         <Link href="/">
           <div id="item-1" className="hidden container:block">
             <TamboluLogo width={logoSize.width} height={logoSize.height}/>
